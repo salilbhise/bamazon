4da95b5f97be5929409525b8c097bbbd505090a1
Your Name <salil_bhise@yahoo.com>
2018-11-04 16:40:11 -0600

    completed js

--- bamazon.sql
+++ bamazon.sql
@@ -11,19 +11,17 @@ stock_quantity INT (50) NOT NULL,
 PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Men's Gucci Logo T-Shirt", "Clothing", 150, 10), ("Men's Wrangler Red Flannel Shirt", "Clothing", 35, 30), ("Men's Champion Hoodie White", "Clothing", 38.23, 6), ("Women's Tom Ford Black Cocktail Dress", "Clothing", 1560, 5), ("Unisex Tommy Hilfiger Dad Hat", "Clothing", 19.99, 25);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Apple iPhone X 64GB Silver", "Electronics", 1299.99, 4), ("Beats by Dre Solo 3 Black", "Electronics", 399.95, 1), ("DJI Inspire 2 Quadcopter Bundle", "Electronics", 20499, 2), ("Firestick w/ Alexa Voice", "Electronics", 39.99, 400), ("Nest T3008US Learning Thermostat", "Electronics", 207.99, 11);
+VALUES ("Apple iPhone X 64GB Silver", "Electronics", 1299.99, 4), ("Beats by Dre Solo 3 Black", "Electronics", 399.95, 1), ("DJI Inspire 2 Quadcopter Bundle", "Electronics", 20499, 2), ("Nest T3008US Learning Thermostat", "Electronics", 207.99, 11);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Pneumatic Standing Desk White", "Furniture", 379, 13), ("Tree Stump Coffee Table", "Furniture", 4750, 3), ("Sonoma 6-Drawer Dresser Black", "Furniture", 198.03, 2), ("Homevibes Vanity Makeup Table", "Furniture", 149.99, 7), ("Eurolux Leather Sofa", "Furniture", 7929, 1);
+VALUES ("Pneumatic Standing Desk White", "Furniture", 379, 13), ("Tree Stump Coffee Table", "Furniture", 4750, 3), ("Homevibes Vanity Makeup Table", "Furniture", 149.99, 7);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Breville BES870XL Barista Espresso Machine", "Kitchen", 597.95, 22), ("OXO Goodgrips Garlic Press", "Kitchen", 15.99, 58), ("Omega J8006 Dual-Stage Juicer", "Kitchen", 224.42, 3), ("Sous Vide Supreme Water Oven", "Kitchen", 428.95, 6), ("Mealthy 5-Blade Spiralizer", "Kitchen", 597.95, 14);
+VALUES ("Breville BES870XL Barista Espresso Machine", "Kitchen", 597.95, 22), ("OXO Goodgrips Garlic Press", "Kitchen", 15.99, 58), ("Omega J8006 Dual-Stage Juicer", "Kitchen", 224.42, 3);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Kewpie Japanese Mayo", "Grocery", 12.95, 17), ("Colmans Original English Mustard", "Grocery", 4.95, 5), ("A5 Japanese Wagyu Ribeye 24oz", "Grocery", 270, 6), ("Now Avocado Oil 16oz", "Grocery", 14.15, 27);
+VALUES ("Kewpie Japanese Mayo", "Grocery", 12.95, 17), ("Colmans Original English Mustard", "Grocery", 4.95, 5));
 
 SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,8 @@
-// required npm packages
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const cTable = require("console.table");
-const colors = require("colors");
+// require npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-// create the connection information for the sql database
+// create connection to mysql database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,23 +11,139 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-// connect to the mysql server and sql database
+// connect to database 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-});
+    console.log("connected as id " + connection.threadId);
+    console.log("I am here");
+    displayProducts();
+  });
+  
+  function displayProducts() {
+    // construct the database query string
+    queryStr = 'SELECT * FROM products';
+  
+    // make the database query
+    connection.query(queryStr, function (err, data) {
+      if (err) throw err;
+  
+      console.log('Existing Inventory:');
+      console.log('........-------...........\n');
+  
+  
+      var strOut = '';
+      for (var i = 0; i < data.length; i++) {
+        strOut = '';
+        strOut += 'Item ID: ' + data[i].item_id + ' | ';
+        strOut += 'Product Name: ' + data[i].product_name + ' | ';
+        strOut += 'Department: ' + data[i].department_name + ' | ';
+        strOut += 'Price: $' + data[i].price + ' | ';;
+        strOut += 'Stock Quantity : ' + data[i].stock_quantity + '\n';
+  
+        console.log(strOut);
+      }
+  
+      console.log("---------------------------------------------------------------------\n\n");
+  
+      // user input for item and quantity they would like to purchase
+  
+      userInput();
+    })
+  
+  }
+  
+  
+  // validateInput ensures that the user is supplying only positive integers for their inputs
+  function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+  
+    if (integer && (sign === 1)) {
+      return true;
+    } else {
+      return 'Please enter a whole non-zero number.';
+    }
+  }
+  
+  function userInput() {
+    inquirer.prompt([
+  
+      {
+        name: "choice",
+        type: "list",
+        choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42'],
+        message: "What item would you like to purchase?"
+      },
+  
+  
+      {
+        type: 'input',
+        name: 'quantity',
+        message: 'What quantity do you need?',
+        validate: validateInput,
+        filter: Number
+      }
+  
+  
+    ]).then(function (input) {
+  
+      let item = input.choice;
+      let quantity = input.quantity;
+  
+      // query database to confirm that the given item ID exists in the desired quantity
+      var queryStr = 'SELECT * FROM products WHERE ?';
+  
+      connection.query(queryStr, {
+        item_id: item
+      }, function (err, data) {
+        if (err) throw err;
+        // log all results of the SELECT statement
+        var productData = data[0];
+  
+        if (productData.stock_quantity < quantity) {
+          console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+          console.log('Please modify your order.\n');
+          displayProducts();
+  
+        } else {
 
-// display items for sale from mysql database
-var purchaseItem = function () {
-        connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function (error, result) {
-                if (error) throw error;
-                var table = new Table({
-                    head: ["ID", "Product", "Department", "Price", "Stock Quantity"]
-                });
-                console.log("Current stock of items for sale");
-                for (var i = 0; i < result.length; i++) {
-                    table.push([result[i].item_id, result[i].product_name, result[i].department_name, result[i].price.toFixed(2), result[i].stock_quantity])
-                }
-                console.log(table.toString());
-            }
+          // construct the updating query string
+
+          var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+          // console.log('updateQueryStr = ' + updateQueryStr);
+  
+          // update inventory
+          connection.query(updateQueryStr, function (err, data) {
+            if (err) throw err;
+            console.log("\n\n---------------------------------------------------------------------");
+            console.log('Your order has been placed! Your total is $' + productData.price * quantity);
+            console.log('Thank you for your order!');
+            console.log('---------------------------\n');
+            purchaseAgain();
+          })
         }
+      });
+    })
+  }
+  
+  function purchaseAgain() {
+    inquirer.prompt([
+  
+      {
+        name: "yesOrno",
+        type: "list",
+        message: "Would you like to purchase another item (yes or no)?",
+        choices: ["yes", "no"]
+      }
+    ]).then(function (answer) {
+
+      // based on their answer, either call the bid or the post functions
+      if (answer.yesOrno.toUpperCase() === "NO") {
+        console.log("Exiting the bamazon customer app");
+        console.log('\nGoodbye!')
+        connection.end();
+      } else {
+        displayProducts();
+      }
+    })
+  }
